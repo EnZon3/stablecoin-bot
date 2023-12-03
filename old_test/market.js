52d2a0421d009fa917ff892bf4cda14015bf14d3
EnZon3 <37979038+EnZon3@users.noreply.github.com>
2023-12-03 22:06:54 +0000

    LIVE TRADING

--- old_test/market.js
+++ old_test/market.js
@@ -1,25 +1,84 @@
-const fs = require('fs');
+const fs = require("fs");
+const crypto = require("crypto");
+const qs = require("qs");
+const axios = require("axios");
 
-const buyToken = async (price, portfolio) => {
-	const amountToBuy = 1000;
-	if (portfolio.USD >= amountToBuy * price) {
-		portfolio.USD -= amountToBuy * price;
-		portfolio.USDT += amountToBuy;
+const api_url = "https://api.kraken.com";
 
-		console.log(`[TRD] >> Bought USDT at ${price} USD`);
-		fs.writeFileSync('save.json', JSON.stringify(portfolio));
+const api_key = process.env['API_KEY'];
+const api_secret = process.env['API_SECRET'];
+
+const getMessageSignature = (path, request, secret, nonce) => {
+	const message       = qs.stringify(request);
+	const secret_buffer = new Buffer(secret, 'base64');
+	const hash          = new crypto.createHash('sha256');
+	const hmac          = new crypto.createHmac('sha512', secret_buffer);
+	const hash_digest   = hash.update(nonce + message).digest('binary');
+	const hmac_digest   = hmac.update(path + hash_digest, 'binary').digest('base64');
+
+	return hmac_digest;
+};
+
+const krakenRequest = (uriPath, data, apiKey, apiSecret) => {
+	const nonce = data.nonce;
+	const headers = {
+		"API-Key": apiKey,
+		"API-Sign": getMessageSignature(uriPath, data, apiSecret, nonce),
+	};
+	return axios.post(api_url + uriPath, qs.stringify(data), {
+		headers: headers,
+	});
+};
+
+const trade = (type, volume) => {
+	const data = {
+		"pair": "USDTZEUR",
+		"type": type,
+		"ordertype": "market",
+		"volume": volume,
+		"nonce": Math.floor(Math.random() * 1000000),
+	};
+	return krakenRequest("/0/private/AddOrder", data, api_key, api_secret);
+};
+
+const buyToken = async (price) => {
+	const balance = await krakenRequest(
+		"/0/private/Balance",
+		{
+			nonce: `${new Date().getTime() + 1}`,
+		},
+		api_key,
+		api_secret
+	);
+	const portfolio = balance.data.result;
+
+	if (portfolio.ZUSD >= amountToBuy * price) {
+		trade('buy', amountToBuy).then(response => {
+			console.log(`[TRD] >> Bought USDT at ${price} USD\n`, response.data.result.descr.order);
+		}).catch(error => {
+			console.error('[TRD] >> Error buying USDT:', error);
+		});
 	} else {
-		console.log('no money AAAAH')
+		console.log("no money AAAAH");
 	}
 };
 
-const sellToken = async (price, portfolio) => {
+const sellToken = async (price) => {
+	const balance = await krakenRequest(
+		"/0/private/Balance",
+		{
+			nonce: `${new Date().getTime() + 1}`,
+		},
+		api_key,
+		api_secret
+	);
+	const portfolio = balance.data.result;
+	
 	if (portfolio.USDT > 0) {
-		portfolio.USD += portfolio.USDT * price;
-
-		console.log(`[TRD] >> Sold all USDT at ${price} USD`);
-		portfolio.USDT = 0;
-		fs.writeFileSync('save.json', JSON.stringify(portfolio));
+		trade('sell', portfolio.USDT).then(response => {
+			console.log(`[TRD] >> Sold all USDT at ${price} USD\n`, response.data.result.descr.order);
+		})
+		.catch(error => console.error('[TRD] >> Error selling USDT:', error));
 	}
 };
 
--- old_test/oldind.js
+++ old_test/oldind.js
@@ -1,64 +1,76 @@
-const WebSocket = require('ws');
-const ws = new WebSocket('wss://ws.kraken.com');
-const market = require('./market.js');
+const WebSocket = require("ws");
+const ws = new WebSocket("wss://ws.kraken.com");
+const market = require("./market.js");
 
-const algorithm = require('./alg2.js');
+const algorithm = require("./alg2.js");
 const min = (minutes) => minutes * 60000;
 let aggregator = new algorithm.DataAggregator(min(0.5), 20);
 
 let portfolio = {
-	USD: 2000,
-	USDT: 0
+  USD: 2000,
+  USDT: 0,
 };
 
 let pivot;
 let high;
 let low;
 
-ws.on('open', function open() {
-	console.log('[TRD] >> Connected');
-	const subscribeMessage = {
-		"event": "subscribe",
-		"pair": [
-			"USDT/EUR"
-		],
-		"subscription": {
-			"name": "trade"
-		}
-	};
+ws.on("open", function open() {
+  console.log("[TRD] >> Connected");
+  const subscribeMessage = {
+    event: "subscribe",
+    pair: ["USDT/EUR"],
+    subscription: {
+      name: "trade",
+    },
+  };
 
-	ws.send(JSON.stringify(subscribeMessage));
+  ws.send(JSON.stringify(subscribeMessage));
 });
 
-ws.on('message', function incoming(data) {
-	try {
-		const jsonData = JSON.parse(data);
-		if (jsonData.event && jsonData.event === 'systemStatus') {
-			console.log(`[WSS] >> Stream API ${jsonData.status} | Kraken API version ${jsonData.version}`)
-		} else if (jsonData[2] === 'trade' && jsonData[3] === 'USDT/EUR' && aggregator.ready === true) {
-			const trades = jsonData[1];
-			const trade = trades[trades.length - 1];
-			const currentPrice = parseFloat(trade[0]);
+ws.on("message", function incoming(data) {
+  try {
+    const jsonData = JSON.parse(data);
+    if (jsonData.event && jsonData.event === "systemStatus") {
+      console.log(
+        `[WSS] >> Stream API ${jsonData.status} | Kraken API version ${jsonData.version}`
+      );
+    } else if (
+      jsonData[2] === "trade" &&
+      jsonData[3] === "USDT/EUR" &&
+      aggregator.ready === true
+    ) {
+      const trades = jsonData[1];
+      const trade = trades[trades.length - 1];
+      const currentPrice = parseFloat(trade[0]);
 
-			console.log(`[TRD] >> Current Price: ${currentPrice} EUR`);
+      console.log(`[TRD] >> Current Price: ${currentPrice} EUR`);
 
-			[pivot, high, low] = Object.values(algorithm.fetchData(aggregator.dataArr));
+      [pivot, high, low] = Object.values(
+        algorithm.fetchData(aggregator.dataArr)
+      );
 
-			console.log(`[TRD] Action: ${currentPrice > high ? 'sell' : currentPrice < low ? 'buy' : 'hold'}`);
-			console.log(`[TRD] >> Pivot: ${pivot} EUR\n[TRD] >> High: ${high} EUR\n[TRD] >> Low: ${low} EUR`)
+      console.log(
+        `[TRD] Action: ${
+          currentPrice > high ? "sell" : currentPrice < low ? "buy" : "hold"
+        }`
+      );
+      console.log(
+        `[TRD] >> Pivot: ${pivot} EUR\n[TRD] >> High: ${high} EUR\n[TRD] >> Low: ${low} EUR`
+      );
 
-			// logic for buying and selling
-			if (currentPrice >= high.toFixed(4)) {
-				market.sellToken(currentPrice, portfolio);
-			} else if (currentPrice <= low.toFixed(4)) {
-				market.buyToken(currentPrice, portfolio);
-			}
-		}
-	} catch (e) {
-		console.log('Error:' + e)
-	}
+      // logic for buying and selling
+      if (currentPrice >= high.toFixed(4)) {
+        market.sellToken(currentPrice, portfolio);
+      } else if (currentPrice <= low.toFixed(4)) {
+        market.buyToken(currentPrice, portfolio);
+      }
+    }
+  } catch (e) {
+    console.log("Error:" + e);
+  }
 });
 
-ws.on('close', function close() {
-	console.log('[TRD] >> Disconnected');
+ws.on("close", function close() {
+  console.log("[TRD] >> Disconnected");
 });
